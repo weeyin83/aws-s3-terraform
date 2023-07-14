fb91a0ae0a2150a4f4649be071607a616e935d8f
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2023-07-14 13:37:10 +0100

    template

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+
+  required_version = ">= 0.13"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+# Bucket creation
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "sarah-terraform-bucket"
+}
+
+
+# Disabling bucket public access
+resource "aws_s3_bucket_public_access_block" "my_s3_bucket_access" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+
+
+  # Block public access
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
